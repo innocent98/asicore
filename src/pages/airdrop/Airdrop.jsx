f69a5ce12b9edddda8c9ba69b44cbb78fce79e3f
innocent98 <tosinadebayo55@gmail.com>
2023-03-03 23:58:04 +0000

    updated

--- src/pages/airdrop/Airdrop.jsx
+++ src/pages/airdrop/Airdrop.jsx
@@ -1,7 +1,65 @@
 import Topbar from "../../components/topbar/Topbar";
 import "./airdrop.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import {
+  loginFailure,
+  loginStart,
+  loginSuccess,
+  logout,
+} from "../../redux/userRedux";
+import { userRequest } from "../../redux/requestMethod";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Airdrop = () => {
+  const user = useSelector((state) => state.user.currentUser);
+  const { isFetching } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const path = location.search;
+  // console.log(path);
+
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleAirdrop = async (e) => {
+    e.preventDefault();
+    dispatch(loginStart());
+    try {
+      const res = await axios.post(
+        path
+          ? "http://api.asicore.xyz/api/user/register" + path
+          : "http://api.asicore.xyz/api/user/register",
+        { ...inputs }
+      );
+      dispatch(loginSuccess(res.data));
+      alert("You have successfully participated in the airdrop.");
+      // window.location.reload()
+    } catch (error) {
+      console.log(error);
+      dispatch(loginFailure());
+    }
+  };
+  // console.log(inputs);
+
+  const userLogout = async () => {
+    dispatch(logout());
+  };
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      userLogout();
+    }, 3600000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  });
+
   return (
     <div className="airdrop">
       <Topbar />
@@ -34,83 +92,97 @@ const Airdrop = () => {
           </div>
         </section>
 
-        <section>
-          <form className="row">
-            <div className="col">
-              <label htmlFor="">
-                <span>Follow us on Telegram</span>{" "}
-                <span className="follow">follow</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Input your telegram username"
-                required
-              />
-            </div>
-            <div className="col">
-              <label htmlFor="">
-                <span>Follow us on Twitter</span>{" "}
-                <span className="follow">follow</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Input your twitter username"
-                required
-              />
-            </div>
-            <div className="col">
-              <label htmlFor="">Retweet our Airdrop tweet</label>
-              <input
-                type="text"
-                placeholder="(Copy and drop your retweet link)"
-                required
-              />
-            </div>
-            <div className="col">
-              <label htmlFor="">Input your CORE address</label>
-              <input
-                type="text"
-                placeholder="Input your CORE address"
-                required
-              />
-            </div>
-            <button>SUBMIT</button>
-          </form>
-        </section>
+        {!user && (
+          <section>
+            <form className="row" onSubmit={handleAirdrop}>
+              <div className="col">
+                <label htmlFor="">
+                  <span>Follow us on Telegram</span>{" "}
+                  <span className="follow">follow</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Input your telegram username"
+                  required
+                  name="telegram"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col">
+                <label htmlFor="">
+                  <span>Follow us on Twitter</span>{" "}
+                  <span className="follow">follow</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Input your twitter username"
+                  required
+                  name="twitter"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col">
+                <label htmlFor="">Retweet our Airdrop tweet</label>
+                <input
+                  type="text"
+                  placeholder="(Copy and drop your retweet link)"
+                  required
+                  name="link"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col">
+                <label htmlFor="">Input your CORE address</label>
+                <input
+                  type="text"
+                  placeholder="Input your CORE address"
+                  required
+                  name="address"
+                  onChange={handleChange}
+                />
+              </div>
+              <button type="submit" disabled={isFetching}>
+                {isFetching ? "Please wait..." : "SUBMIT"}
+              </button>
+            </form>
+          </section>
+        )}
 
-        <section>
-          <div className="airdropDetails">
-            <h5>
-              Refer Link:{" "}
-              <span>
-                <a href="/">https://asicore.org/QW23E</a>
-              </span>
-            </h5>
-            <p>
-              Copy and share your refer link,you will be rewarded 3 ASICORE and
-              the invitee will be rewarded 0.09ASI at the same time, each person
-              can invite up to 3 ASI token
-            </p>
+        {user && (
+          <section>
+            <div className="airdropDetails">
+              <h5>
+                Refer Link:{" "}
+                <span>
+                  <a href="/">{`http://asicore.xyz/airdrop?_id=${user?._id}`}</a>
+                </span>
+              </h5>
+              <p>
+                Copy and share your refer link,you will be rewarded 3 ASICORE
+                and the invitee will be rewarded 0.09ASI at the same time, each
+                person can invite up to 3 ASI token
+              </p>
 
-            <div className="details">
-              <div className="det">
-                <h5>0.09</h5>
-                <h4>ASICORE</h4>
-                <p>Balance</p>
+              <div className="details">
+                <div className="det">
+                  <h5>{user?.balance}</h5>
+                  <h4>ASICORE</h4>
+                  <p>Balance</p>
+                </div>
+                <div className="det">
+                  <h5>{user?.referred}</h5>
+                  <p>Referred</p>
+                </div>
+                <div className="det">
+                  <h5>{user?.reward}</h5>
+                  <h4>ASICORE</h4>
+                  <p>Rewards</p>
+                </div>
               </div>
-              <div className="det">
-                <h5>0.09</h5>
-                <p>Referred</p>
-              </div>
-              <div className="det">
-                <h5>0.09</h5>
-                <h4>ASICORE</h4>
-                <p>Rewards</p>
-              </div>
+              <button disabled>WITHDRAW</button>
             </div>
-            <button>WITHDRAW</button>
-          </div>
-        </section>
+          </section>
+        )}
 
         <div className="alertBox">
           <div className="title">
