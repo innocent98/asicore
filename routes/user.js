93528b5a9778719418cfd5c9c6d9c455922ff722
innocent98 <tosinadebayo55@gmail.com>
2023-05-14 14:05:51 +0000

    updated

--- routes/user.js
+++ routes/user.js
@@ -37,4 +37,34 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ address: req.body.address });
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res
+        .status(404)
+        .json("Account not found! Please participate in the airdrop first.");
+    }
+  } catch (err) {
+    res.status(500).json("Internal server error");
+  }
+});
+
+// get user
+router.post("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({ address: req.body.address });
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json("Not found!");
+    }
+  } catch (err) {
+    res.status(500).json("Internal server error");
+  }
+});
+
 module.exports = router;
