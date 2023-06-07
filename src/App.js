c020d71d179760e1aeccf9761f3594e13a32784c
innocent98 <tosinadebayo55@gmail.com>
2023-06-07 00:25:57 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ import {
 import { Suspense, lazy } from "react";
 import { useSelector } from "react-redux";
 import Admin from "./pages/admin/Admin";
+import Admin12 from "./pages/admin/Admin12";
+import Admin11 from "./pages/admin/Admin11";
+import Admin9 from "./pages/admin/Admin9";
+import Admin8 from "./pages/admin/Admin8";
+import Admin7 from "./pages/admin/Admin7";
+import Admin6 from "./pages/admin/Admin6";
+import Admin5 from "./pages/admin/Admin5";
+import Admin4 from "./pages/admin/Admin4";
+import Admin3 from "./pages/admin/Admin3";
+import Admin2 from "./pages/admin/Admin2";
+import Admin1 from "./pages/admin/Admin1";
+import Admin10 from "./pages/admin/Admin10";
 const Airdrop = lazy(() => import("./pages/airdrop/Airdrop"));
 const Login = lazy(() => import("./pages/airdrop/Login"));
 const Home = lazy(() => import("./pages/home/Home"));
@@ -35,6 +47,102 @@ function App() {
       element: (
         <Suspense fallback={<div></div>}>
           <Admin />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/asicore-check/1",
+      element: (
+        <Suspense fallback={<div></div>}>
+          <Admin1 />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/asicore-check/2",
+      element: (
+        <Suspense fallback={<div></div>}>
+          <Admin2 />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/asicore-check/3",
+      element: (
+        <Suspense fallback={<div></div>}>
+          <Admin3 />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/asicore-check/4",
+      element: (
+        <Suspense fallback={<div></div>}>
+          <Admin4 />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/asicore-check/5",
+      element: (
+        <Suspense fallback={<div></div>}>
+          <Admin5 />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/asicore-check/6",
+      element: (
+        <Suspense fallback={<div></div>}>
+          <Admin6 />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/asicore-check/7",
+      element: (
+        <Suspense fallback={<div></div>}>
+          <Admin7 />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/asicore-check/8",
+      element: (
+        <Suspense fallback={<div></div>}>
+          <Admin8 />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/asicore-check/9",
+      element: (
+        <Suspense fallback={<div></div>}>
+          <Admin9 />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/asicore-check/10",
+      element: (
+        <Suspense fallback={<div></div>}>
+          <Admin10 />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/asicore-check/11",
+      element: (
+        <Suspense fallback={<div></div>}>
+          <Admin11 />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/asicore-check/12",
+      element: (
+        <Suspense fallback={<div></div>}>
+          <Admin12 />
         </Suspense>
       ),
     },
---
+++ src/pages/admin/Admin2.jsx
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import Box from "@mui/material/Box";
+import axios from "axios";
+
+const Admin2 = () => {
+  const [withdraw, setWithdraw] = useState([]);
+  const [isPaid, setIsPaid] = useState(false)
+
+  const fetchWithdraw = async () => {
+    try {
+      const res = await axios.get(
+        "https://api.asicore.xyz/api/user/withdraw/all"
+      );
+      setWithdraw(res.data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    let unsubscribe = fetchWithdraw();
+    return () => unsubscribe;
+  }, [isPaid]);
+
+  const reversed = [...withdraw].reverse();
+
+  //   handle mark paid
+  const handleMarkPaid = async (id) => {
+    try {
+      await axios.put(`https://api.asicore.xyz/api/user/mark-paid/${id}`);
+      setIsPaid(!isPaid)
+    } catch (error) {}
+  };
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 150 },
+    {
+      field: "twitter",
+      headerName: "Twitter",
+      width: 110,
+    },
+    {
+      field: "telegram",
+      headerName: "Telegram",
+      width: 110,
+    },
+    {
+      field: "link",
+      headerName: "Retweet link",
+      width: 200,
+    },
+    {
+      field: "referred",
+      headerName: "Reward",
+      width: 110,
+    },
+    {
+      field: "balance",
+      headerName: "Balance",
+      width: 110,
+    },
+    {
+      field: "address",
+      headerName: "Wallet Address",
+      width: 300,
+    },
+    {
+      field: "isPaid",
+      headerName: "Paid",
+      width: 120,
+      renderCell: (params) => {
+        return (
+          <button
+            style={{ padding: "10px" }}
+            onClick={() => handleMarkPaid(params.row.id)}
+          >
+            {params.row.isPaid ? "Paid" : "Mark paid"}
+          </button>
+        );
+      },
+    },
+  ];
+
+  const rows = reversed.slice(1251, 2501).map((item) => ({
+    id: item._id,
+    twitter: item.twitter,
+    telegram: item.telegram,
+    link: item.link,
+    referred: item.referred,
+    balance: item.balance,
+    address: item.address,
+    isPaid: item.isPaid,
+  }));
+
+  return (
+    <Box sx={{ height: "100vh", width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 50,
+            },
+          },
+        }}
+        pageSizeOptions={[50]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </Box>
+  );
+};
+
+export default Admin2;
